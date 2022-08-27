88e60b50e413b83c20f732a93abc42488b22092f
Code-With-Hashim <hashim.khan83029@gmail.com>
2022-08-27 20:54:11 +0530

    for Checking

--- Scripts/signup.js
+++ Scripts/signup.js
@@ -12,44 +12,67 @@ let create = async () => {
         email : email,
         password : password,
         username : email,
-        mobile : '#',
+        mobile : email,
         description : 'Welcome'
  
     }
 
+    //hashim.khan481545@gmail.com
+    //hashim0000
+
     //kashim457@gmail.com
     //kasim123
 
-    try {
-        let res = await fetch('https://masai-api-mocker.herokuapp.com/auth/register', {
-        method : 'POST',
-
-        body : JSON.stringify(send_data),
-
-        headers : {
-            'Content-Type' : 'application/json'
-        }
-        
-    }) 
-
-    let data = await res.json()
-    let {error} = data
-    let {message} = data
-
-    console.log(message)
-
-    if(error) {
-        alert(message)
-    } else {
-        alert(message)
-        window.location.href = 'login.html'
-    }
-
-
-    }
-    catch(error) {
-        alert
-    }
     
+    //     let res = await fetch('https://masai-api-mocker.herokuapp.com/auth/register', {
+    //     method : 'POST',
+
+    //     body : JSON.stringify(send_data),
+
+    //     headers : {
+    //         'Content-Type' : 'application/json'
+    //     }
+        
+    // }) 
+
+    // let data = await res.json()
+    // // let {error} = data
+    // // let {message} = data
+
+    // // console.log(message)
+    // console.log(data)
+
+    // // if(error) {
+    // //     alert(message)
+    // // } else {
+    // //     alert(message)
+    // //     window.location.href = 'login.html'
+    
+
+    // let register_data = {
+    //     name: document.getElementById('name').value,
+    //     email: document.getElementById('email').value,
+    //     password: document.getElementById('password').value,
+    //     username: document.getElementById('username').value,
+    //     mobile: document.getElementById('mobile').value,
+    //     description: document.getElementById('description').value,
+    // }
+
+    const res = await fetch('https://masai-api-mocker.herokuapp.com/auth/register', {
+        method: "POST",
+
+        body: JSON.stringify(send_data),
+
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const data = await res.json()
+    console.log(data)
+
+
+
+   
 
 }

4a4421b83164f96d6ed8cbfc39b2943e75759163
GauravSingh72 <1gaurav2singh@gmail.com>
2022-08-28 11:33:46 +0530

    Added signin functionality

--- Scripts/name.js
+++ Scripts/name.js
@@ -1,31 +1,37 @@
-let sinak = JSON.parse(localStorage.getItem("userdetails")) || {};
+let sinak = JSON.parse(localStorage.getItem("userdetails")) || null;
+
+let signin = document.getElementById("signintext");
 
 //hashim.khan48184@gmail.com
 //Hashim124
 
-setTimeout(()=> {
-  localStorage.setItem('userdetails', '' )
-  window.location.reload(true)
-},300000)
+// setTimeout(()=> {
+//   localStorage.setItem('userdetails', '' )
+//   window.location.reload(true)
+// },300000)
 
-let {name} = sinak
+// let {name} = sinak
 
-
-
-let signin = document.getElementById('signintext')
-
-let display = document.getElementById('mySignin')
-
-signin.innerText = name
-
-if (signin.innerText == 'Sign In') {
-  
-} else if(sinak.innerText == undefined) {
-  signin.innerText = 'Sign In' 
-  
+if (sinak == null) {
+  signin.innerText = "Sign In";
 } else {
-  display.style.display = 'none'
+  let { name } = sinak;
+  signin.innerText = name;
+}
+
+let { name } = sinak;
+signin.innerText = name;
+console.log(name);
+
+// let signin = document.getElementById('signintext')
+
+let display = document.getElementById("mySignin");
+
+signin.innerText = name;
+
+if (signin.innerText == "Sign In") {
+} else {
+  display.style.display = "none";
 }
 
 //display.style.display = 'none'
-
